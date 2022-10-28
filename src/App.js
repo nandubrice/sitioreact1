f244c67dda4af65123c706a1362678d5928cc039
Arnado Briceño Catacora <bricenoarnaldo7@gmail.com>
2022-10-27 19:35:03 -0500

    V02 my-app1 Se ha modificado

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          ARNALDO BRICEÑO CATACORA
+          ARNALDO BRICEÑO
         </a>
       </header>
     </div>
