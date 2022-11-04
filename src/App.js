972c2b2ab201c0ead2c69b87f64e908fffe9fcb0
Arnado Briceño Catacora <bricenoarnaldo7@gmail.com>
2022-11-03 20:54:09 -0500

    V02 estoy implementando funciones para registrar en BD

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
+import { useState } from "react";
 import AppForm from "./componentes/AppForm";
 
 function App() {
+///////////////////LECTURA  (READ) ///////////////////////////
+
+const [idActual, setIdActual ] = useState('');    //EL IDACTUAL SU FUNCION ES : para actualizar y eliminar
+const [docsBD, setDocsBD] = useState([]);        //para lectura a BD
+
+////////////LECTURA A BASE DE DATOS////////////
+const fnRead = () =>{
+  console.log("Lectura a BD");
+} 
+
+////////////LECTURA A BASE DE DATOS////////////
+const fnDelete = () =>{
+  console.log("Eliminar un registro");
+} 
+
   return (
     <div style={{background:"greenyellow", width:"350px"}}>
-      <AppForm />
+      <AppForm {...{idActual, setIdActual, fnRead}} />
     </div>
   );
 }
--- src/componentes/AppForm.js
+++ src/componentes/AppForm.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const AppForm = () => {
+const AppForm = (props) => {
+
+    const camposRegistro={nombre:"", edad:"", genero:""};   //estructura de la tabla(campos para los registros)
+    const [objeto, setObjeto] = useState(camposRegistro);   //tabla a objeto(define un estado)
+
+    const controlarEstadoCambio = (e) => {          //maneja cambios en imput
+    };
+    const controlSubmit = (e) => {                   // maneja submit (envio)
+         
+    };
   return (
     <div style={{background:"orange"}}>
         Formulario AppForm.js
