dfa725783403e252ef868379d159304fd0c12beb
Arnado Briceño Catacora <bricenoarnaldo7@gmail.com>
2022-11-03 20:13:26 -0500

    actualizado

---
+++ src/componentes/AppForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppForm = () => {
+  return (
+    <div style={{background:"orange"}}>
+        Formulario AppForm.js
+    </div>
+  )
+}
+
+export default AppForm
---
+++ src/componentes/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyCX9ZWiESNHxBTEXJdi4mEEVtcJAjJf22U',
+    authDomain: 'fb-bdreact1-e42f5.firebaseapp.com',
+    projectId: 'fb-bdreact1-e42f5',
+    storageBucket: "fb-bdreact1-e42f5.appspot.com",
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore();
+export default app;
